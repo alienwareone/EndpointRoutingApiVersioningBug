316a73ad6175d655acdb40fa86d70f7a4ed45b61
alienwareone <beni@mailboxes.ch>
2020-09-03 17:05:14 +0200

    Projektdateien hinzufügen.

---
+++ EndpointRoutingApiVersioningBug/Controllers/ValuesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace EndpointRoutingApiVersioningBug.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0", Deprecated = true)]
+    [ApiVersion("1.1", Deprecated = true)]
+    [ApiVersion("2.0")]
+    [Route("api/values")]
+    [Route("api/v{version:apiVersion}/values")]
+    public class ValuesController : Controller
+    {
+        [HttpGet(nameof(GetValues))]
+        public async Task<object> GetValues()
+        {
+            await Task.CompletedTask;
+            return HttpContext.GetRequestedApiVersion();
+        }
+
+        [MapToApiVersion("1.1")]
+        [HttpGet(nameof(GetValues))]
+        public async Task<object> GetValues_V1_1()
+        {
+            await Task.CompletedTask;
+            return HttpContext.GetRequestedApiVersion();
+        }
+
+        [MapToApiVersion("2.0")]
+        [HttpGet(nameof(GetValues))]
+        public async Task<object> GetValues_V2_0()
+        {
+            await Task.CompletedTask;
+            return HttpContext.GetRequestedApiVersion();
+        }
+    }
+}
---
+++ EndpointRoutingApiVersioningBug/Startup.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace EndpointRoutingApiVersioningBug
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddApiVersioning(x =>
+            {
+                x.DefaultApiVersion = new ApiVersion(1, 0);
+                x.AssumeDefaultVersionWhenUnspecified = true;
+                x.ReportApiVersions = true;
+                x.ApiVersionReader = ApiVersionReader.Combine(
+                    new HeaderApiVersionReader(),
+                    new UrlSegmentApiVersionReader(),
+                    new QueryStringApiVersionReader(),
+                    new MediaTypeApiVersionReader());
+            });
+
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
